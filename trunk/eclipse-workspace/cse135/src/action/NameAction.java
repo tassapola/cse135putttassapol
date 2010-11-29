ff868e28faca864ba943d086e6e13eca5ed3d010
chilltassapola <chilltassapola@16fcbdfc-56bc-7f80-26f2-9ba74a5772c8>
2010-11-29 08:40:13 +0000

    add set citizenship list

--- trunk/eclipse-workspace/cse135/src/action/NameAction.java
+++ trunk/eclipse-workspace/cse135/src/action/NameAction.java
@@ -11,7 +11,7 @@ public class NameAction extends Action {
 
 	public ActionForward execute(ActionMapping mapping, ActionForm form,
 								 HttpServletRequest request,
-								 HttpServletResponse response) {
+								 HttpServletResponse response) throws Exception {
 		Applicant a = (Applicant) request.getSession().getAttribute(Constants.ATTR_NAME_APPLICANT);
 		if (a == null) {
 			a = new Applicant();
