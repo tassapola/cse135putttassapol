8507787fc19e92e82bdc0e6f85a4e1c142dc4bfa
chilltassapola <chilltassapola@16fcbdfc-56bc-7f80-26f2-9ba74a5772c8>
2010-11-29 09:09:58 +0000

    up citizenship.jsp

--- trunk/eclipse-workspace/cse135/src/model/Applicant.java
+++ trunk/eclipse-workspace/cse135/src/model/Applicant.java
@@ -7,10 +7,34 @@ public class Applicant {
 	private String lastName;
 	private String middleName;
 	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
 	public void setNames(NameForm f) {
 		firstName = f.getFirstName();
 		lastName = f.getLastName();
-		middleName = f.getLastName();
+		middleName = f.getMiddleName();
 		//System.out.println("set firstName = " + firstName);
 	}
 }
