66f57244bdf6aa55ca35aa57b5d4bd21e717ae30
chilltassapola <chilltassapola@16fcbdfc-56bc-7f80-26f2-9ba74a5772c8>
2010-11-29 07:00:30 +0000

    add app src

---
+++ eclipse-workspace/cse135/src/application/NameAction.java
@@ -0,0 +1,14 @@
+package application;
+
+import javax.servlet.http.*;
+import org.apache.struts.action.*;
+
+
+public class NameAction extends Action {
+
+	public ActionForward execute(ActionMapping mapping, ActionForm form,
+								 HttpServletRequest request,
+								 HttpServletResponse response) {
+		return mapping.findForward("success");
+	}
+}
---
+++ eclipse-workspace/cse135/src/application/NameForm.java
@@ -0,0 +1,49 @@
+package application;
+
+import javax.servlet.http.*;
+import org.apache.struts.action.*;
+
+public class NameForm extends ActionForm {
+
+	private String firstName = null;
+	private String lastName = null;
+	private String middleName = null;
+		
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public ActionErrors validate(ActionMapping mappping,
+								 HttpServletRequest request
+								) {
+		ActionErrors errors = new ActionErrors();
+		//System.out.println("validating");
+		//System.out.println(firstName + " " + lastName);
+		if (firstName.equals("") || lastName.equals("")) {
+			errors.add("err1", new ActionMessage("errors.required", "first name/last name"));
+			//System.out.println("adding error");
+		}
+		//TODO: Check for digits
+		return errors;
+	}
+}
