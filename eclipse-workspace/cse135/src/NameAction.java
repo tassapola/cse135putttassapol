331bc1bcfb8a1ab870a926ae953777aac7c31597
chilltassapola <chilltassapola@16fcbdfc-56bc-7f80-26f2-9ba74a5772c8>
2010-11-29 03:32:07 +0000

    update src

---
+++ eclipse-workspace/cse135/src/NameAction.java
@@ -0,0 +1,12 @@
+import javax.servlet.http.*;
+import org.apache.struts.action.*;
+
+
+public class NameAction extends Action {
+
+	public ActionForward execute(ActionMapping mapping, ActionForm form,
+								 HttpServletRequest request,
+								 HttpServletResponse response) {
+		return mapping.findForward("success");
+	}
+}
