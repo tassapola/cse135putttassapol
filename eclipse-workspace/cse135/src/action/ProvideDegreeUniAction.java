df96b10b3909216e7f3252497e19e2ca0ed99146
chilltassapola <chilltassapola@16fcbdfc-56bc-7f80-26f2-9ba74a5772c8>
2010-11-30 10:01:53 +0000

    fix a bug for check GPA

--- eclipse-workspace/cse135/src/action/ProvideDegreeUniAction.java
+++ eclipse-workspace/cse135/src/action/ProvideDegreeUniAction.java
@@ -21,20 +21,6 @@ public class ProvideDegreeUniAction extends Action {
 		//System.out.println("setting university = " + ((ProvideDegreeUniForm) form).getUniversity());
 		
 
-		Class.forName("org.postgresql.Driver");
-		Connection con=DriverManager.getConnection("jdbc:postgresql://localhost/cse135?user=postgres&password=password");
-		con.setAutoCommit(false);
-		PreparedStatement stmt = con.prepareStatement("SELECT name FROM disciplines WHERE id <= 4 ORDER BY name asc;");
-		
-		ResultSet disciplines = stmt.executeQuery();
-		LinkedList<String> l = new LinkedList<String>();
-		while(disciplines.next()) {
-			String discipline = disciplines.getString("name");
-			l.add(discipline);
-		}
-		stmt.close();
-		//System.out.println("disciplines list size = " + l.size());
-		request.setAttribute(Constants.LIST_DISCIPLINES, l);
 		return mapping.findForward(Constants.FORWARD_SUCCESS);
 	}
 }
