ba2d09d638693fe0cec41bde7d7fa69858fb6df8
chilltassapola <chilltassapola@16fcbdfc-56bc-7f80-26f2-9ba74a5772c8>
2010-12-01 00:23:55 +0000

    revert Putt's specializationaction

--- eclipse-workspace/cse135/src/action/SpecializationAction.java
+++ eclipse-workspace/cse135/src/action/SpecializationAction.java
@@ -1,28 +1,40 @@
 package action;
 
 import javax.servlet.http.*;
+import org.apache.struts.action.*;
 import model.*;
 
-import org.apache.struts.action.*;
+import java.sql.*;
+import java.util.ArrayList;
 
-import form.NameForm;
-import form.ResidencyForm;
-import form.SpecializationForm;
-
-public class SpecializationAction extends Action {
-
+public class SpecializationAction extends Action 
+{
 	public ActionForward execute(ActionMapping mapping, ActionForm form,
 								 HttpServletRequest request,
-								 HttpServletResponse response) throws Exception {
-		SpecializationForm f = (SpecializationForm) form;
-		Applicant a = (Applicant) request.getSession().getAttribute(Constants.ATTR_NAME_APPLICANT);
-		if (a == null) {
-			a = new Applicant();
-		}
-		a.setSpecialization(f.getSpecialization());
-		//System.out.println("specialization = " + f.getSpecialization());
-		request.getSession().setAttribute(Constants.ATTR_NAME_APPLICANT, a);
+								 HttpServletResponse response) throws Exception 
+    {
+		Class.forName("org.postgresql.Driver");
+		Connection con=DriverManager.getConnection("jdbc:postgresql://localhost/cse135?user=postgres&password=password");
+		Statement stmt = con.createStatement();
 		
+		ResultSet sp = stmt.executeQuery("" +
+				"SELECT s.name AS name, " + 
+				"COUNT(DISTINCT a.id) AS count FROM applicant a, specializations s " + 
+				"WHERE a.specialization = s.id " +
+				"GROUP BY s.name"); 
+		
+		ArrayList<String> s = new ArrayList<String>();
+		ArrayList<Integer> c = new ArrayList<Integer>();
+		while(sp.next())
+		{
+			s.add(sp.getString(1));
+			c.add(sp.getInt(2));
+		}
+		
+		request.setAttribute("specialization", s);
+		request.setAttribute("count", c);
+				
 		return mapping.findForward(Constants.FORWARD_SUCCESS);
 	}
 }
+
